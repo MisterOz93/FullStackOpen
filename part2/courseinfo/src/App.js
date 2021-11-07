caf6b1e2682af340a988b6d5a2da00dc07666672
Michael Mroz <mroz15@hotmail.com>
2021-11-06 23:26:28 -0400

    exercise 2.1 complete

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,34 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+const Course = ({course}) => {
   return(
-    <p>Number of exercises {sum}</p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
-    </div>
+    <>
+    <h1>{course.name}</h1>
+    {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p> )}
+    </>
   )
 }
 
@@ -51,16 +29,10 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
-      }
+      },
     ]
   }
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
+  return <Course course={course}/>
 }
 export default App; 
