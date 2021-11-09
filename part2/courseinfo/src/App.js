32f438fde182b235028fb985b956465a98adfdb8
Michael Mroz <mroz15@hotmail.com>
2021-11-08 23:29:52 -0500

    2.4 complete

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -5,7 +5,7 @@ const Course = ({course}) => {
   const total = course.parts.reduce(function (sum, part){return sum + part.exercises}, 0)
   return(
     <>
-    <h1>{course.name}</h1>
+    <h2>{course.name}</h2>
     {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p> )}
     <p><strong>Total of {total} exercises</strong></p>
 
@@ -14,33 +14,60 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    { 
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course}/>
+  return (
+    <div>
+      <h1>Web Development Curriculum</h1>
+      <ul>
+    {courses.map(course => <Course course={course} key={course.id} />)}
+
+      </ul>
+
+    </div>
+  )
 }
 export default App; 
