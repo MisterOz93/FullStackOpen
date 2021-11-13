8f22b464a0c3952c4e27e9ab31c1448f95a3d105
Michael Mroz <mroz15@hotmail.com>
2021-11-13 00:42:37 -0500

    2.8 complete

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456'},
+    { name: 'Ada Lovelace', number: '39-44-5323523'},
+    { name: 'Dan Abramov', number: '12-43-234345'},
+    { name: 'Mary Poppendieck', number: '39-23-6423122'}
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
+  const [ search, setSearch] = useState('')
 
   const changeName = (event) => {
     setNewName(event.target.value)
@@ -15,6 +18,14 @@ const App = () => {
   const changeNumber = (event) => {
     setNewNumber(event.target.value)
   }
+
+  const changeSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const filteredPersons = search === ''
+    ? persons 
+    : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -25,7 +36,7 @@ const App = () => {
     if (persons.map(person => person.name).includes(newName)){
       alert(`${newName} is already in the phonebook.`)
     }
-    else if(persons.map(person => persons.number).includes(newNumber)){
+    else if(persons.map(person => person.number).includes(newNumber)){
       alert(`${newNumber} is already registered in the phonebook.`)
     }
     else {
@@ -37,6 +48,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        Show only names containing <input value={search} onChange={changeSearch}/>
+        </form>
+      <h2>Add a new: </h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={changeName}/> </div>
@@ -49,7 +64,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul> 
-        {persons.map(person => <li key={person.name}>{(person.name)} {person.number}</li>)}
+        {filteredPersons.map(person => <li key={person.name}>{(person.name)} {person.number}</li>)}
       </ul>
     </div>
   )
