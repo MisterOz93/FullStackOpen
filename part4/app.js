2027306cffdf51de4c72c5fef1fcc6053b7fc580
Michael Mroz <mroz15@hotmail.com>
2022-02-08 00:16:40 -0500

    moved route handling to controllers dir

--- part4/app.js
+++ part4/app.js
@@ -5,15 +5,23 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
+logger.info('Connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI).then(
+  () => {
+    logger.info('Connected to MongoDB')
+  }
+).catch((error) => {
+  logger.error('Error connecting to MongoDB', error.message)
+})
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+app.use(cors())
 app.use(morgan('tiny'))
 
-app.get('/api/blogs', (request, response) => {
+/* app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
@@ -30,5 +38,5 @@ app.post('/api/blogs', (request, response) => {
       response.status(201).json(result)
     })
 })
-
+*/
 module.exports = app
