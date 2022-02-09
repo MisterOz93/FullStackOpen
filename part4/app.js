620d3a2189ed986e2f593645940d17f7d60055cb
Michael Mroz <mroz15@hotmail.com>
2022-02-08 23:06:51 -0500

    4.2 done, refactored into seperate modules, fixed blog.rest POST to include title field

--- part4/app.js
+++ part4/app.js
@@ -4,11 +4,12 @@ const cors = require('cors')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 logger.info('Connecting to', config.MONGODB_URI)
+
 mongoose.connect(config.MONGODB_URI).then(
   () => {
     logger.info('Connected to MongoDB')
@@ -16,27 +17,12 @@ mongoose.connect(config.MONGODB_URI).then(
 ).catch((error) => {
   logger.error('Error connecting to MongoDB', error.message)
 })
+
 app.use(express.json())
+//app.use(express.static('build')) //not built yet
 app.use('/api/blogs', blogsRouter)
 app.use(cors())
+app.use(middleware.errorHandler)
 app.use(morgan('tiny'))
 
-/* app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-*/
 module.exports = app
--- part4/index.js
+++ part4/index.js
@@ -1,8 +1,9 @@
 const app = require('./app')
 const logger = require('./utils/logger')
 const http = require('http')
-const server = http.createServer(app);
 const config = require('./utils/config')
+
+const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
