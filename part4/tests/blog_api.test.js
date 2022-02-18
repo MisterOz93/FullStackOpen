a85bfea9eee997c9bd4e398c5bae0db063ba33da
Michael Mroz <mroz15@hotmail.com>
2022-02-17 23:14:47 -0500

    updated logger to not interfere w tests and added async/await to blog_api test:

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,14 +1,48 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const initialState = [
+  {
+    title:'How to blog',
+    author:'Bloggert McBlogface',
+    url:'www.blog.com',
+    likes: 1
+  },
+  {
+    title:'Every movie that ever existed',
+    author: 'Humphrey Bloggart',
+    url: 'www.casablog.ca',
+    likes: 1500000,
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialState[0])
+  await blogObject.save()
+  blogObject = new Blog(initialState[1])
+  await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialState.length)
+})
+
+test('the first blog is called: How to blog', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].title).toBe('How to blog')
 })
 
 afterAll(() => {
