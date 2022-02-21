b2550edc4063371de9ad0a566fd7b716fb084f0a
Michael Mroz <mroz15@hotmail.com>
2022-02-21 00:12:45 -0500

    4.8 to 4.10 complete. test added to ensure _id was changed to id

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -15,21 +15,29 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+describe('using GET method', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('there are two blogs', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(helper.initialState.length)
-})
+  test('there are two blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialState.length)
+  })
 
-test('the first blog is called: How to blog', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body[0].title).toBe('How to blog')
+  test('the first blog is called: How to blog', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].title).toBe('How to blog')
+  })
+
+  test('blog identifier is called \'id\' instead of _id', async() => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+    expect(response.body[0]._id).toBeUndefined()
+  })
 })
 
 test('a new blog can be added', async () => {
