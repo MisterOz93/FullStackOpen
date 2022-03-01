8501334c20ae7b22a7c929ff048fa7ffaedc4196
Michael Mroz <mroz15@hotmail.com>
2022-02-28 23:50:07 -0500

    recovering from git error

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -40,39 +40,52 @@ describe('using GET method', () => {
   })
 })
 
-test('a new blog can be added', async () => {
-  const newBlog = {
-    title: 'Blogging off',
-    author: 'Foo McFooson',
-    url: 'McFoosonblog.com',
-    likes: 1
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-type', /application\/json/)
+describe('using POST method', () => {
+  test('a new blog can be added', async () => {
+    const newBlog = {
+      title: 'Blogging off',
+      author: 'Foo McFooson',
+      url: 'McFoosonblog.com',
+      likes: 1
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-type', /application\/json/)
 
-  const response = await api.get('/api/blogs')
-  const authors = response.body.map(r => r.author)
-  expect(response.body).toHaveLength(helper.initialState.length + 1)
-  expect(authors).toContain('Foo McFooson')
-})
+    const response = await api.get('/api/blogs')
+    const authors = response.body.map(r => r.author)
+    expect(response.body).toHaveLength(helper.initialState.length + 1)
+    expect(authors).toContain('Foo McFooson')
+  })
+  test('blog with missing likes dataField gets defaulted to 0', async () => {
+    const blogWithoutLikes = {
+      title: 'How to cope with popularity',
+      author: 'Johnny Kickass',
+      url: 'www.bloggingelite.com',
+    }
+    await api.post('/api/blogs')
+      .send(blogWithoutLikes)
+      .expect(201)
+      .expect('Content-type', /application\/json/)
 
-test('blog with missing likes dataField gets defaulted to 0', async () => {
-  const blogWithoutLikes = {
-    title: 'How to cope with popularity',
-    author: 'Johnny Kickass',
-    url: 'www.bloggingelite.com',
-  }
-  await api.post('/api/blogs')
-    .send(blogWithoutLikes)
-    .expect(201)
-    .expect('Content-type', /application\/json/)
-
-  const postedBlogs = await helper.blogsInDB()
-  const addedBlog = postedBlogs[postedBlogs.length -1]
-  expect(addedBlog.likes).toEqual(0)
+    const postedBlogs = await helper.blogsInDB()
+    const addedBlog = postedBlogs[postedBlogs.length -1]
+    expect(addedBlog.likes).toEqual(0)
+  })
+  test.only('POSTing blog with no title or url returns status 400', async () => {
+    const blogWithoutTitleOrURL = {
+      author: 'Sloppy Joe',
+      likes: 2425358
+    }
+    await api.post('/api/blogs')
+      .send(blogWithoutTitleOrURL)
+      .expect(400)
+      .expect('Content-type', /application\/json/)
+    const blogs = await helper.blogsInDB()
+    expect(blogs.length).toEqual(helper.initialState.length)
+  })
 })
 
 afterAll(() => {
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,7 +1,10 @@
 const logger = require('./logger')
 
 const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
+  if (error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
+  }
+  logger.error(error.name)
   next(error)
 }
 module.exports = {
