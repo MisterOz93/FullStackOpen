59bc20fd2831bc72c9f29852df86109ce9e8a00a
Michael Mroz <mroz15@hotmail.com>
2022-03-26 22:55:12 -0400

    commented out incorrect test

--- part4/tests/login_api.test.js
+++ part4/tests/login_api.test.js
@@ -5,5 +5,20 @@ const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const api = supertest(app)
 
-
+describe('with a single user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    const user = new User({ username: 'Foo', password: 'pw'})
+    await user.save()
+  }, 15000)
+  /* does not work currently need to fix
+    test('log in successful with proper name & pw', async () => {
+    const data = await User.find({})
+    const found = await User.findOne( data[0])
+    console.log(found)
+    await api.post('/api/login')
+      .send(data[0])
+      .expect(200)
+  }) */
+})
 afterAll( () => mongoose.connection.close())
